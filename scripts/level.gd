d54fafbde2d462c564e4fd7e310a81cfaafb2a9e
SrPatto <ladrianrv@hotmail.com>
2025-03-25 13:39:24 -0600

    Prototype
    - player basic move
    - player death
    - basic obstacles
    - 6 modules
    - level generator

--- scripts/level.gd
+++ scripts/level.gd
@@ -4,14 +4,21 @@ extends Node2D
 var amount = 10
 var randomNumber = RandomNumberGenerator.new()
 var offset = 640
+var initObstacles = 0
 
 func _ready() -> void:
 	for n in amount:
 		spawn_module(n*offset)
 
 func spawn_module(n):
-	randomNumber.randomize()
-	var num = randomNumber.randi_range(0, modules.size()-1)
-	var instance = modules[num].instantiate()
-	instance.position.x = n
-	add_child(instance)
+	if initObstacles > 2:
+		randomNumber.randomize()
+		var num = randomNumber.randi_range(0, modules.size()-1)
+		var instance = modules[num].instantiate()
+		instance.position.x = n
+		add_child(instance)
+	else:
+		var instance = modules[0].instantiate()
+		instance.position.x = n
+		add_child(instance)
+		initObstacles += 1
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-@onready var ray_cast_2d: RayCast2D = $RayCast2D
+@onready var ray_cast_platform = $RayCast_Platform
+@onready var ray_cast_death_sensor = $RayCast_DeathSensor
 
 const RUN_SPEED = 300.0
 const JUMP_VELOCITY = -500.0
@@ -10,13 +11,23 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
-	# Handle jump.
+	# Handle jump up.
 	if Input.is_action_just_pressed("jump_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
-	if ray_cast_2d.is_colliding():
-		var collision = ray_cast_2d.get_collider()
+	move_and_slide()
+	
+func _process(delta):
+	# Handle jump down.
+	if ray_cast_platform.is_colliding():
+		var collision = ray_cast_platform.get_collider()
 		if collision.has_method("disable") and Input.is_action_just_pressed("jump_down"):
 			collision.disable()
-	
-	move_and_slide()
+	# Handle death sensor
+	if ray_cast_death_sensor.is_colliding():
+		var collision = ray_cast_death_sensor.get_collider()
+		if collision.is_in_group("obstacles"):
+			death()
+
+func death():
+	get_tree().reload_current_scene()
