5d075f673f35ad029b6d25da054c369698e4defc
SrPatto <ladrianrv@hotmail.com>
2025-03-31 18:30:11 -0600

    hotfix music

--- scripts/music.gd
+++ scripts/music.gd
@@ -6,7 +6,6 @@ var isPlayingGameplayMusic
 func _ready():
 	isPlayingGameplayMusic = false
 	music = self.get_stream_playback()
-	print(music)
 
 func _process(delta):
 	if get_parent().gameRunning:
