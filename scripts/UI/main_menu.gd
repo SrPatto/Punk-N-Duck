30ed7e375c9c2efdbede06033f645b9e6c738a5b
SrPatto <ladrianrv@hotmail.com>
2025-04-09 09:06:36 -0600

    Version 1.1
    Fix UI
    Return button in menu

--- scripts/UI/main_menu.gd
+++ scripts/UI/main_menu.gd
@@ -4,3 +4,8 @@ extends CanvasLayer
 @onready var start_label: Label = $Menu_Container/StartLabel
 @onready var play_button: Button = $Menu_Container/Buttons_Container/Play_Button
 @onready var leaderboard_button: Button = $Menu_Container/Buttons_Container/Leaderboard_Button
+@onready var return_button = $ReturnButton
+
+
+func _on_return_button_pressed():
+	SceneTransition.change_scene("res://scenes/UI/start_screen.tscn")
